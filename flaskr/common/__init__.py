9845e60c9f931bf0e5de61d0416b21b6257bc4e8
mijing <544495538@qq.com>
2019-12-12 00:52:02 +0800

    modified:   flaskr/common/__init__.py 	modified:   flaskr/templates/index.html 	modified:   flaskr/templates/member-list.html 	modified:   flaskr/web/api.py

--- flaskr/common/__init__.py
+++ flaskr/common/__init__.py
@@ -2,10 +2,14 @@
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import socket
 
 # 配置数据库 todo 迁移到 dotenv
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:dev2312@127.0.0.1/api_wechat?charset=utf8mb4'
+if socket.gethostname() == 'bcpmai-win10':
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:dev2312@127.0.0.1/api_wechat?charset=utf8mb4'
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://baernaoer:baernaoer@127.0.0.1/yijiemp?charset=utf8mb4'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db_session = SQLAlchemy(app).session
