08af37f1d92a4c0022b2e020ddfaf798aa53f5df
bcpmai <bcpmai@126.com>
2019-12-13 22:02:46 +0800

    modified:   flaskr/web/api.py

--- flaskr/web/api.py
+++ flaskr/web/api.py
@@ -448,6 +448,8 @@ def weixin_pay():
 
         rep = client_post_xml_data_requests(url, xml_data)
 
+        print(data)
+        print(rep.text)
         ss = rep.text.encode('raw_unicode_escape')
         print(ss.decode())
 
