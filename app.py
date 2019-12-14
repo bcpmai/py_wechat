b156694e7b969778b2d8fdaed99e1282d4da296a
mijing <544495538@qq.com>
2019-12-14 15:04:45 +0800

    modified:   app.py

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import os
+import socket
 
 from flaskr import create_app
 
@@ -7,4 +9,10 @@ app = create_app()
 
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5000, debug=True)
+    # app.run(host='127.0.0.1', port=5000, debug=True)
+    if socket.gethostname() == 'bcpmai-win10':
+        app.run(host='127.0.0.1', port=5000, debug=True)
+    else:
+        ssl_crt = os.path.join(os.path.dirname(app.instance_path), '3189443_yijie.xiusha.net_public.crt')
+        ssl_key = os.path.join(os.path.dirname(app.instance_path), '3189443_yijie.xiusha.net.key')
+        app.run(host='127.0.0.1', port=5000, debug=True, ssl_context=(ssl_crt, ssl_key))
